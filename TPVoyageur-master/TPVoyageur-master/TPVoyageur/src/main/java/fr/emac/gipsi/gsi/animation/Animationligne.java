211793ed2f291f0ac3b184dc8531a2b065f95da8
Kotteya <charles.kotteya_wattage@mines-albi.fr>
2020-06-05 14:38:24 +0200

    coucou

---
+++ TPVoyageur-master/TPVoyageur-master/TPVoyageur/src/main/java/fr/emac/gipsi/gsi/animation/Animationligne.java
@@ -0,0 +1,11 @@
+package fr.emac.gipsi.gsi.animation;
+
+public class Animationligne extends AbstractAnimation {
+
+	@Override
+	public void runAnimation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
