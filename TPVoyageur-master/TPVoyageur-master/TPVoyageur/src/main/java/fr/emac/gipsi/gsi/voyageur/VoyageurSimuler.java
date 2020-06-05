46a7e34918fe1ef9f734e26bce7d05b2fe239362
Gayoux <theo.gayoux@mines-albi.fr>
2020-06-05 17:17:20 +0200

    05/06/2020

--- TPVoyageur-master/TPVoyageur-master/TPVoyageur/src/main/java/fr/emac/gipsi/gsi/voyageur/VoyageurSimuler.java
+++ TPVoyageur-master/TPVoyageur-master/TPVoyageur/src/main/java/fr/emac/gipsi/gsi/voyageur/VoyageurSimuler.java
@@ -3,12 +3,15 @@
  */
 package fr.emac.gipsi.gsi.voyageur;
 
+import fr.emac.gipsi.gsi.voyage.Position;
+
 /**
  * @author Truptil Sebastien - sebastien.truptil@gmail.com
  *
  */
 public class VoyageurSimuler extends AbstractVoyageur {
-
+	
+			
     /**
      *
      */
@@ -21,16 +24,122 @@ public class VoyageurSimuler extends AbstractVoyageur {
      */
     @Override
     protected void forward() {
+    	
+    	String direction = getDirection();
+    	
+    	if (direction == "N") {
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()-1);
+    		nouvellePositionB.setX(getPosBody().getX()-1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    		
+    	}
+    	else if (direction == "S") {
+    		setPosTete(Position(getPosTete().getX()-2,getPosTete().getY()));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()-1);
+    		nouvellePositionB.setX(getPosBody().getX()-1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    		
+    	}
+    	else if (direction == "E") {
+    		setPosTete(Position(getPosTete().getX()-1,getPosTete().getY()-1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()-1);
+    		nouvellePositionB.setX(getPosBody().getX()-1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    	
+   
+    	}
+    	else if (direction == "O") {
+    		setPosTete(Position(getPosTete().getX()-1,getPosTete().getY()+1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()-1);
+    		nouvellePositionB.setX(getPosBody().getX()-1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    	}
+    	
         // TODO Auto-generated method stub
+        setDirection("N");
 
     }
+    private Position Position(int i, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    /* (non-Javadoc)
+	/* (non-Javadoc)
      * @see fr.emac.gipsi.gsi.voyageur.AbstractVoyageur#backward()
      */
     @Override
     protected void backward() {
+    	
+    	String direction = getDirection();
+    	
+    	if (direction == "N") {
+    		
+    		setPosTete(Position(getPosTete().getX()+2,getPosTete().getY()));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()+1);
+    		nouvellePositionB.setX(getPosBody().getX()+1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    	}
+    	else if (direction == "S") {
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()+1);
+    		nouvellePositionB.setX(getPosBody().getX()+1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    		
+    	}
+    	else if (direction == "E") {
+    		setPosTete(Position(getPosTete().getX()+1,getPosTete().getY()-1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()+1);
+    		nouvellePositionB.setX(getPosBody().getX()+1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    	
+   
+    	}
+    	else if (direction == "O") {
+    		setPosTete(Position(getPosTete().getX()+1,getPosTete().getY()+1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX()+1);
+    		nouvellePositionB.setX(getPosBody().getX()+1);
+    		nouvellePositionT.setY(getPosTete().getY());
+    		nouvellePositionB.setY(getPosBody().getY());
+    	}
         // TODO Auto-generated method stub
+        setDirection("S");
 
     }
 
@@ -39,14 +148,129 @@ public class VoyageurSimuler extends AbstractVoyageur {
      */
     @Override
     protected void left() {
-        // TODO Auto-generated method stub
-    }
+    	String direction = getDirection();
+    	
+    	if (direction == "N") {
+    		
+    		setPosTete(Position(getPosTete().getX()+1,getPosTete().getY()-1));
 
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()-1);
+    		nouvellePositionB.setY(getPosBody().getY()-1);
+    	}
+    	else if (direction == "S") {
+    
+    	
+    		setPosTete(Position(getPosTete().getX()-1,getPosTete().getY()-1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()-1);
+    		nouvellePositionB.setY(getPosBody().getY()-1);
+    		
+    	}
+    	else if (direction == "E") {
+    		setPosTete(Position(getPosTete().getX(),getPosTete().getY()-2));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()-1);
+    		nouvellePositionB.setY(getPosBody().getY()-1);
+    	
+   
+    	}
+    	else if (direction == "O") {
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()-1);
+    		nouvellePositionB.setY(getPosBody().getY()-1);
+        // TODO Auto-generated method stub
+        
+    	setDirection("O");
+
+    }
+    }
     /* (non-Javadoc)
      * @see fr.emac.gipsi.gsi.voyageur.AbstractVoyageur#right()
      */
     @Override
+    
     protected void right() {
-        // TODO Auto-generated method stub
+    	
+    	String direction = getDirection();
+    	
+    	if (direction == "N") {
+    		
+    		setPosTete(Position(getPosTete().getX()+1,getPosTete().getY()+1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()+1);
+    		nouvellePositionB.setY(getPosBody().getY()+1);
+    	}
+    	else if (direction == "S") {
+    
+    	
+    		setPosTete(Position(getPosTete().getX()-1,getPosTete().getY()+1));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()+1);
+    		nouvellePositionB.setY(getPosBody().getY()+1);
+    		
+    	}
+    	else if (direction == "E") {
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()+1);
+    		nouvellePositionB.setY(getPosBody().getY()+1);
+    	
+   
+    	}
+    	else if (direction == "O") {
+    		
+    	}
+    		setPosTete(Position(getPosTete().getX(),getPosTete().getY()+2));
+
+    		Position nouvellePositionT = new Position();
+    		Position nouvellePositionB = new Position();
+
+    		nouvellePositionT.setX(getPosTete().getX());
+    		nouvellePositionB.setX(getPosBody().getX());
+    		nouvellePositionT.setY(getPosTete().getY()-1);
+    		nouvellePositionB.setY(getPosBody().getY()-1);
+    	
+        setDirection("E");
+
     }
-}
+        // TODO Auto-generated method stub*
+    	
+    	
+    	
+
+    }
+
